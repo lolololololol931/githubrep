5569e636b0c0e4f4882e15108d3b2b1d41d5b976
lolololololol931 <kirill2008tuy@gmail.com>
2024-11-30 17:29:40 +0200

    masiv vkazivnik

--- githubrep/githubrep.cpp
+++ githubrep/githubrep.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     system("chcp 1251>null");
-    cout << "вказівники\n";
+    /*cout << "вказівники\n";
     int a = 2, b = 5;
     cout << &a << " " << &b << endl;
 
@@ -16,6 +16,16 @@ int main()
     cout << ptr << endl;
     cout << a << endl;
     ptr = &b;
-    cout << *ptr << endl;
+    cout << *ptr << endl;*/
+
+    int ar[] = { 1,8,9,4,10,7,10 };
+    int size = sizeof(ar) / sizeof(ar[0]);
+    cout << size << endl;
+    cout << *ar << endl;
+    cout << &ar[0] << endl;
+    for (int i = 0; i < size; i++) {
+        /*cout << &ar+i << " ";*/
+        cout << *(ar + i) << " ";
+    }
 
 }
