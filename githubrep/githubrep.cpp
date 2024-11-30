6a1fe6deab1f8a3db2132e0263bf897d3f30d7b6
lolololololol931 <kirill2008tuy@gmail.com>
2024-11-30 17:14:01 +0200

    vkazivniki

--- githubrep/githubrep.cpp
+++ githubrep/githubrep.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    system("chcp 1251>null");
+    cout << "вказівники\n";
+    int a = 2, b = 5;
+    cout << &a << " " << &b << endl;
+
+    int* ptr = nullptr;
+    ptr = &a;
+    cout << ptr<<endl;
+    *ptr = 11;
+    cout << *ptr << endl;
+    cout << ptr << endl;
+    cout << a << endl;
+    ptr = &b;
+    cout << *ptr << endl;
+
 }
